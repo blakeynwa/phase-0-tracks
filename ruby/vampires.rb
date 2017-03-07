3876ced0902cfe6eab49e992486bc913b20dcb6b
Blake Meyer <blake.ynwa@gmail.com>
2017-03-07 14:51:35 -0800

    Update language on the questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,16 +3,15 @@ wolves_like_garlic = true
 vampires_like_sunshine = false
 vampires_like_garlic = true
 currentyear = 2017
-valid_input = "yes" || "no"
 
 puts "What is your name?"
   name = gets.chomp
 puts "How old are you? What year were you born?"
   age = gets.chomp
   ageyear = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes or no)"
   garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (yes or no)"
   insurance = gets.chomp
 
 if 
