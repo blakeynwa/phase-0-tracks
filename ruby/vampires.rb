b694324cf5c2cc624fdf03c9e02301b1a20c42f2
Blake Meyer <blake.ynwa@gmail.com>
2017-03-07 12:51:24 -0800

    Add vampire detection logic to age and year

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,30 @@
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = true
+currentyear = 2017
+
 puts "What is your name?"
   name = gets.chomp
 puts "How old are you? What year were you born?"
+  age = gets.chomp
   ageyear = gets.chomp
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
   garlic = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
   insurance = gets.chomp
+
+if 
+  ageyear.to_i == currentyear - age.to_i
+  puts "Probably not a vampire"
+else
+    puts "Probably a vampire"
+end
+
+
+
+
+
+
+
+
