ee1f5053af459f8b40bcfaa7b3d9a01c5a5bcbaa
Blake Meyer <blake.ynwa@gmail.com>
2017-03-07 20:11:55 -0800

    Add loop to check for number of employees and clean up code (i.e. remove to_i when not necessary)

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,32 +4,38 @@ vampires_like_sunshine = false
 vampires_like_garlic = true
 currentyear = 2017
 
-puts "What is your name?"
-  name = gets.chomp
-puts "How old are you?"
-  age = gets.chomp
-puts "What year were you born?"
-  ageyear = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes or no)"
-  garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance? (yes or no)"
-  insurance = gets.chomp
+puts "How many employees will be processed at this time?"
+  employeesnumber = gets.chomp.to_i
 
-if 
-  name == "Drake Cula" || name == "Tu Fang"
-  puts "Definitely a vampire"
-elsif
-  (ageyear.to_i != currentyear - age.to_i) && (garlic == "yes" || insurance == "yes")
-  puts "Probably a vampire"
-elsif 
-  (ageyear.to_i != currentyear - age.to_i) && (garlic == "no" && insurance == "no")
-  puts "Almost certainly a vampire"
-elsif 
-  (ageyear.to_i == currentyear - age.to_i) && (garlic == "yes" || insurance == "yes")
-  puts "Probably not a vampire"
-else
-  puts "Results Inconclusive"
+until employeesnumber == 0
+  employeesnumber = employeesnumber - 1
+  puts "What is your name?"
+    name = gets.chomp
+  puts "How old are you?"
+    age = gets.chomp.to_i
+  puts "What year were you born?"
+    ageyear = gets.chomp.to_i
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes or no)"
+    garlic = gets.chomp
+  puts "Would you like to enroll in the company's health insurance? (yes or no)"
+    insurance = gets.chomp
+  if 
+    name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire"
+  elsif
+    (ageyear != currentyear - age) && (garlic == "yes" || insurance == "yes")
+    puts "Probably a vampire"
+  elsif 
+    (ageyear != currentyear - age) && (garlic == "no" && insurance == "no")
+    puts "Almost certainly a vampire"
+  elsif 
+    (ageyear == currentyear - age) && (garlic == "yes" || insurance == "yes")
+    puts "Probably not a vampire"
+  else
+    puts "Results Inconclusive"
+  end
 end
+
 
 
 
