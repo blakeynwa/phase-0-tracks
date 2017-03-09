5d98d838bb3fc72e4d3fc99a4e60c986df61ea9c
Blake Meyer <blake.ynwa@gmail.com>
2017-03-09 10:03:26 -0800

    Add first outlines for secret agents encryption

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,11 @@
+# figure out the index of the letter and increase the index by 1 each loop
+
+secret_word = "abc def"
+index = 0
+
+while index < secret_word.length
+  p secret_word[index].next
+  index += 1
+end
+
+
