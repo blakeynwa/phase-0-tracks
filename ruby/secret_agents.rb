df40be9e2c387a3a5c786d15512203e108007db3
Blake Meyer <blake.ynwa@gmail.com>
2017-03-09 11:14:38 -0800

    Add decryption functionality

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,5 +1,5 @@
 # figure out the index of the letter and increase the index by 1 each loop
-def encrypt (string)
+def encrypt(string)
   new_word = ""
   index = 0
   
@@ -15,4 +15,26 @@ def encrypt (string)
   new_word
 end
 
+def decrypt(encrypted_word)
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+
+word = ""
+index  = 0
+new_word = encrypted_word.split("")
+  
+  while index < encrypted_word.length
+    alphabet_indexes = alphabet.index(new_word[index])
+    
+    alphabet_indexes
+      if alphabet_indexes != 26
+        word += alphabet[alphabet_indexes -1]
+      else
+        word << " "
+      end
+      index += 1
+  end
+  word 
+end
+
+# Successfully can call nested method because both data types used are strings
 
