da1482c4fbfa54a1d13e80a7e62c097c309ddc42
Blake Meyer <blake.ynwa@gmail.com>
2017-03-09 10:27:34 -0800

    Add encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,18 +1,18 @@
 # figure out the index of the letter and increase the index by 1 each loop
-
-secret_word = "abc def"
-new_word = ""
-index = 0
-
-while index < secret_word.length
-  if secret_word[index] != " "
-    new_word += secret_word[index].next
-  else
-    new_word << " "
+def encrypt (string)
+  new_word = ""
+  index = 0
+  
+  while index < string.length
+    if string[index] != " "
+      new_word += string[index].next
+    else
+      new_word << " "
+    end
+    index += 1
   end
-  index += 1
+  
+  new_word
 end
-
-p new_word
 
 
