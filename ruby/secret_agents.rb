2173103efa9fe7218654fe3616cb1b18a6425293
Blake Meyer <blake.ynwa@gmail.com>
2017-03-09 10:24:09 -0800

    Complete encryption portion

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,11 +1,18 @@
 # figure out the index of the letter and increase the index by 1 each loop
 
 secret_word = "abc def"
+new_word = ""
 index = 0
 
 while index < secret_word.length
-  p secret_word[index].next
+  if secret_word[index] != " "
+    new_word += secret_word[index].next
+  else
+    new_word << " "
+  end
   index += 1
 end
+
+p new_word
 
 
