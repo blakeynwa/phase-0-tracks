718862cefeb7b5e249e1e8559a1212dc4812973b
Blake Meyer <blake.ynwa@gmail.com>
2017-03-14 22:56:54 -0700

    Add more pseudo code and verify functionality of required code

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,17 +7,19 @@ form = {}
 #questions prompting the user to input data into the hash
 puts "Please enter your client's name"
   form[:name] = gets.chomp
-puts "Age"
+puts "Age?"
   form[:age] = gets.chomp.to_i
-puts "Number of children"
+puts "Number of children?"
   form[:num_of_child] = gets.chomp.to_i
-puts "Decor Theme"
+puts "Decor Theme?"
   form[:decor_theme] = gets.chomp
 
 #print the form back to the user
 puts form
 
 #Give the user the option of updating the form
+#If user does not want to update the form, say thank you 
+#If user wants to update the form, ask them for the key they want to update
 puts "Which information needs updating? (Type label name or none)"
   user_input = gets.chomp
   if user_input == "none"
@@ -26,7 +28,10 @@ puts "Which information needs updating? (Type label name or none)"
     puts "please enter the new value"
     form[user_input.intern] = gets.chomp
   end
+
+#Print the updated form 
+puts form 
   
-  puts form 
-  
+
+
 
