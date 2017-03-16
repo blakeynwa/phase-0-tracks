e6532a1dfb2b001ea6d468ace61f3261db864d1b
Blake Meyer <blake.ynwa@gmail.com>
2017-03-16 10:41:46 -0700

    Verify fibonacci sequence works

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+=begin
 def search_array(arr, int)
   idx = 0
   while idx < arr.length
@@ -11,14 +12,30 @@ def search_array(arr, int)
 end
 
 
+arr = [42, 89, 23, 1]
+p search_array(arr, 1)
 
+p search_array(arr, 24)
+
+=end
+
+#The number given as an argument represents number of integers in array
+#When calling fib method, return should be length of given argument in an
+#array and fibonacci sequence of integers
+
+def fib(int)
+  initial = [0, 1]
+  until initial.length == int 
+    update = (initial[-2] + initial[-1])
+    initial << update
+  end
+  return initial
+end
+
+p fib(6)
 
 
 
 
  
 
-arr = [42, 89, 23, 1]
-p search_array(arr, 1)
-
-p search_array(arr, 24)
