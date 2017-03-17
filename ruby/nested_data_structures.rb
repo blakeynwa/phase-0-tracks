c7807ceebb7fdeb94b4eb3923d6e5258205667dd
Blake Meyer <blake.ynwa@gmail.com>
2017-03-16 21:00:59 -0700

    Add sophomore section

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -9,7 +9,18 @@ classroom_supplies = {
           "Pencils",
           "Textbooks"]
 
+    },
+    room2: {
+        name: "Sophomore",
+        number_of_students: {
+          total_capacity: 40,
+          current_number: 39
+        },
+        supplies_needed: [
+          "Stickie Notes",
+          "Projector"]
     }
 }
 
 p classroom_supplies[:room1][:supplies_needed][0]
+p classroom_supplies[:room2][:number_of_students]
