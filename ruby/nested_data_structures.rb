1c855a7f5c8c6b7156054e96111f63e3464e2e72
Blake Meyer <blake.ynwa@gmail.com>
2017-03-16 21:07:06 -0700

    Finish adding room names and contents

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -19,8 +19,30 @@ classroom_supplies = {
         supplies_needed: [
           "Stickie Notes",
           "Projector"]
-    }
+    },
+    room3: {
+        name: "Junior",
+        number_of_students: {
+          total_capacity: 37,
+          current_number: 21
+        },
+        supplies_needed: [
+          "Soccer balls",
+          "Desks"]
+    },
+    room4: {
+        name: "Senior",
+        number_of_students: {
+          total_capacity: 20,
+          current_number: 3
+        },
+        supplies_needed: [
+          "Attendance Sheet",
+          "Senioritis Anditdote"]
+    },
 }
 
-p classroom_supplies[:room1][:supplies_needed][0]
-p classroom_supplies[:room2][:number_of_students]
+puts classroom_supplies[:room1][:supplies_needed][0]
+puts classroom_supplies[:room2][:number_of_students]
+puts classroom_supplies[:room3][:name]
+puts classroom_supplies[:room4][:supplies_needed][1]
