7cb7b5ea065a0519a4aa8753e18f234fa8110326
Blake Meyer <blake.ynwa@gmail.com>
2017-03-19 14:27:02 -0700

    Add printing of variables to ensure funcionality

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,7 +3,33 @@
 #rotate method, that takes the index number given as count and 
 #returns that index as the first in the array.
 
+
+
+puts "What's your first name?"
+    first_name = gets.chomp 
+  puts "What's your last name?"
+    last_name = gets.chomp 
+
 def nameswap(first_name, last_name)
   array = [first_name, last_name]
-  new_array = array.rotate!(count = 1)
+  newarray = array.rotate!(count = 1)
 end
+
+swapped_name = nameswap(first_name, last_name).join(" ")
+
+p swapped_name
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
