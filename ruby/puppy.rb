eeaeb9137bc3dc1b0abeef3d23d506dbb1f13c3a
Blake Meyer <blake.ynwa@gmail.com>
2017-03-20 10:14:11 -0700

    Add puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#Breed--------------
+#Golden Retriever
+
+#Characteristics----
+#Fur color: gold
+#friendly: yes
+#good dog: yes
+#name: varies
+
+#Behavior-----------
+#Sporadic
+#Fun loving
+#Roll over
+#play dead
