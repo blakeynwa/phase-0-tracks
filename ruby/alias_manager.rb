f4ae025698724b1861909196a3884f9cc9b713ef
Blake Meyer <blake.ynwa@gmail.com>
2017-03-19 19:37:56 -0700

    Define methods for vowel checking and consonant checking, add identification for uppercase v lowercase letters, ensure functionality of the code through release 1

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,11 +8,14 @@ end
 puts nameswap(name)
 
 def vowelchange(string)
-vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+vowels = ['a', 'e', 'i', 'o', 'u']
+vowelscap = ['A', 'E', 'I', 'O', 'U']
 originalname = nameswap(string).split("")
 vowel_name = originalname.map do |letter|
   if vowels.include? letter
-    "a"
+    vowels[vowels.index(letter) + 1]
+  elsif vowelscap.include? letter
+    vowelscap[vowelscap.index(letter) + 1]
   else
     letter
   end
@@ -24,16 +27,21 @@ end
 def conschange(string)
   consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
                 "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  conscap = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M",
+                "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
   preconscheck = vowelchange(string)
   cons_name = preconscheck.map do |letter|
-    if consonants.include? letter
-      "y"
+    if consonants.include? letter 
+      consonants[consonants.index(letter) + 1]
+    elsif conscap.include? letter
+      conscap[conscap.index(letter) + 1]
     else
       letter
     end
   end
 end
 
-p conschange("blake meyer")
-  
+
+
+p conschange("Felicia Torres")
   
