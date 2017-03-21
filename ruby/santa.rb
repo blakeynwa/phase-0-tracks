9b449cde9b2bbf9be510303efafed0881e4a341b
Blake Meyer <blake.ynwa@gmail.com>
2017-03-21 09:38:50 -0700

    Add new methods under the do the thing section

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,10 @@
 class Santa
 
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -8,11 +13,18 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
-  def initialize
-    puts "Initializing Santa instance..."
+  def santaage
+    age = 0
   end
   
+  def reindeer
+    reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
+                "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  end
 
 end
 
-Santa.new.eat_milk_and_cookies("snickerdoodle")
+santa = Santa.new("straight", "white")
+santa.speak
+santa.eat_milk_and_cookies("snickerdoodle")
+puts santa.reindeer
