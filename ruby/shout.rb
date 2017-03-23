8f5eb8bd5951b33af90a17acb209e6ca7be6012d
Blake Meyer <blake.ynwa@gmail.com>
2017-03-23 09:58:07 -0700

    Add yell_happily and yell_angrily, call using driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,13 @@
 module Shout
-  #fill
+  def self.yell_angrily(words)
+    puts words + "!!!" + ":("
+  end
+
+  def self.yell_happily(words)
+    puts words.upcase + "!!!" + ";D"
+  end
+
 end
 
+Shout.yell_happily("yay!")
+Shout.yell_angrily("NO")
