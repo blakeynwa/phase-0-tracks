4b52a51bc9e6273b7f314711906d814a7f5c5c9a
Blake Meyer <blake.ynwa@gmail.com>
2017-03-23 10:06:41 -0700

    Add shout method as mixin, call with driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -16,3 +16,29 @@ Shout.yell_happily("yay!")
 Shout.yell_angrily("NO")
 =end 
 
+module Shout
+  def yell_angrily(words)
+    puts words + "!!!" + ":("
+  end
+
+  def yell_happily(words)
+    puts words.upcase + "!!!" + ";D"
+  end
+
+end
+
+class Person
+  include Shout
+end
+
+class Dragon
+  include Shout
+end
+
+person = Person.new
+person.yell_angrily("nope")
+person.yell_happily("yay")
+
+dragon = Dragon.new
+dragon.yell_happily("woop")
+dragon.yell_angrily("ugh")
