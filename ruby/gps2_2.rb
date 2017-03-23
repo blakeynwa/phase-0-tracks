52069d3c8dcb989468d06e21051c4e078a35e527
Blake Meyer <blake.ynwa@gmail.com>
2017-03-22 23:18:17 -0700

    Add reflection

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -73,7 +73,44 @@ grocery_items.each do |item, quantity|
   puts "#{item}, #{quantity}"
 end
 
+=begin
+  
+What did you learn about pseudocode from working on this challenge?
+I first thought it would be a good idea to go through and update the pseudo
+code as we went along, but now I'm seeing it's a much better idea to have
+detailed pseudo code to read through as you start to actually code. 
+Having detailed pseudo code will lead to more functional actual code.
 
+What are the tradeoffs of using arrays and hashes for this challenge?
+At first we were going to use an array, but then realized it would be 
+pretty difficult to access both the items and the quantities. Using an 
+array allowed us to use the items as 'keys' and the quantities as 'values', 
+which made it much easier to access and update the various elements. 
+
+What does a method return?
+A method returns the result of the last code in the method. 
+
+What kind of things can you pass into methods as arguments?
+You can pass strings, integers, arrays/hashes
+
+How can you pass information between methods?
+As stated at the start of this challenge, one way we could have done this
+was with classes. However, we performed this in this challenge by using 
+the return of previous methods as the argument for new methods. For example, 
+we defined a method user_list first, which populated a list of grocery items
+with a default quantity of 1. The return of that method was a list, which we 
+used in future methods such as item_add(list, item, quantity). This allowed
+us to use the list populated from user_list in all the other methods we
+created. 
+
+What concepts were solidified in this challenge, and what concepts are still confusing?
+Passing information from one method to another was cleared up. It's still
+a confusing topic since so much of this information is new to me, but I feel
+more confident on passing information from one method to the other moving forward.
+It was also nice having additional practice with hashes/arrays, but I definitely
+need continued practice to fully wrap my brain around data structures. 
+  
+end
 
 
   
