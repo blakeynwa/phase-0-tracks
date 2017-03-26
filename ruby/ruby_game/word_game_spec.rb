f189129015b8768c9af6f3e31c66dc73b1d27f0c
Blake Meyer <blake.ynwa@gmail.com>
2017-03-26 12:17:48 -0700

    Continue buildout of rspec testing

--- ruby/ruby_game/word_game_spec.rb
+++ ruby/ruby_game/word_game_spec.rb
@@ -3,8 +3,18 @@ require_relative 'word_game'
 describe GameOfWords do 
   let(:word_game) {GameOfWords.new("toast")}
 
-  it "encrypts a secret word and display asterisks" do 
-    expect(word_game.word_comparison("t")).to eq "t***t"
+  it "accepts a letter as a guess" do 
+    expect(word_game.word_comparison("t")).to eq ["t"]
   end
+
+  it "puts the guess limit equivalent to the secret word length plus 2" do
+    expect(word_game.guess_limit).to eq 7
+  end
+
+  it "puts asterisks in place of secret word given for user to guess" do 
+    expect(word_game.secret_guess).to eq ["*", "*", "*", "*", "*"]
+  end
+
+
 
 end
