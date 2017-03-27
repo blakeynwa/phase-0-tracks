b39712c1ce75327890cce281436a26d4bc1925da
Blake Meyer <blake.ynwa@gmail.com>
2017-03-27 16:59:00 -0700

    Refactor method parameters

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -20,14 +20,14 @@ class VirusPredictor
   #determines predicted death (specific number of people) and 
   #speed of spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
   #takes all three instance variables and returns number of deaths
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -46,7 +46,7 @@ class VirusPredictor
   end
 
   #takes in population density and state, returns the speed of death
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
