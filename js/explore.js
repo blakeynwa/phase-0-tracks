3a7a30d1d27f96c9e23c3380557dc3a557e424e8
Blake Meyer <blake.ynwa@gmail.com>
2017-03-28 08:37:18 -0700

    Add pseudo code for reverse string function

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+//Pseudocode
+//define a function called reverse
+//After doing some research through MDN, I realized there is indeed
+//a built in reverse in js - but it's for arrays, not strings.
+//Thinking similar to ruby, I'm going to split the string so each 
+//characters goes into an array. Then I'll reverse the array, which 
+//should return the array with the characters reversed, and finally 
+//I'll join the array into a single string, which should provide the reversed
+//string with no spaces. 
