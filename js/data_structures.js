69926afac60355fa483317362a16b09f35d2b0c4
Blake Meyer <blake.ynwa@gmail.com>
2017-03-30 09:50:39 -0700

    Create object with names = colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,10 @@ var names = ["Tom", "Greg", "Jerry", "Horse"]
 
 colors.push("Mauve");
 names.push("Donald");
+
+horseChar = {}
+for (var i = 0; i < names.length; i++) {
+  horseChar[names[i]] = colors[i];
+}
+
+console.log(horseChar)
