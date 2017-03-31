337dd21ebb1d25ecc5c65392d81503259176a1ce
Blake Meyer <blake.ynwa@gmail.com>
2017-03-30 21:05:21 -0700

    Add pseudocode for longest word sort

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+/*Create an array with words or phrases of various length using var longWords
+For the quickest way to access the longest word, sort the strings in the array
+in descending order based on length. What this will look like: if the array contains
+the words "toast", "hi", "computer", and "coffee", the sort function will change the 
+array order to "hi", "toast", "coffee", and "computer". In order to accomplish this, 
+create a sort function that compares strings in their indexes (in this case function
+(a, b)). In the function, pass a parameter that is "return a.length - b.length," that 
+will sort the array in descending order of length. According to the MDN resources, 
+if the outcome of a.length - b.length is negative, it will sort 'a' to a lower index
+than 'b', which is exactly what we're trying to accomplish. Once sorting is complete, 
+log the last index string to the console using pop, which returns the final index of 
+an array. */
+
